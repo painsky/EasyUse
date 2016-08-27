509c79d8d0b0939bbffb80112babb831a6d6e68a
pain <skyshenfu@gmail.com>
2016-08-27 17:36:01 +0800

    Initial commit

---
+++ app/src/main/java/com/learn/easyuse/MainActivity.java
@@ -0,0 +1,31 @@
+package com.learn.easyuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+       button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Toast.makeText(MainActivity.this, "显示内容", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
